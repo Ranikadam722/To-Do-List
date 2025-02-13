ceb484208086c6c407daaf99678337f512bedcfd
Ranikadam722 <kadamrani722@gmail.com>
2025-02-13 22:33:21 +0530

    Add files via upload

---
+++ ToDolist.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.todolist;
+
+/**
+ *
+ * @author kadam
+ */
+public class ToDolist {
+
+    public static void main(String[] args) {
+        System.out.println("To - Do List!");
+    }
+}
